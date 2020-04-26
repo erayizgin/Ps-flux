2621ae69889a4b37004c45afed459ad5ca9db5e3
erayizgin <erayizgin@gmail.com>
2020-04-26 21:43:53 +0300

    react forms, links, navlink, switch route

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 // tavsiye edilen ve kullanılan component oluşturma biçimi. (function component). 
 // eski versiyonlar sadece class component desteklemekte.
 function HomePage() {
     return (
-    <div className="jumbotron">
-        <h1>Pluralsight Administration</h1>
-        <p>React, Flux and React Router for ultra responsive web apps.</p>
-        <a href="/about">About</a>
-    </div>
+        <div className="jumbotron">
+            <h1>Pluralsight Administration</h1>
+            <p>React, Flux and React Router for ultra responsive web apps.</p>
+            <Link to="about" className="btn btn-primary">About</Link>
+        </div>
     );
 }
 
